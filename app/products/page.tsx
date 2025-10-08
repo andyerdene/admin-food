3ed41a4d93ddb0b4072025a5bb42215bda4a8fc8
andyerdene <andyerdene@gmail.com>
2025-10-08 12:41:56 +0800

    changes

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -10,15 +10,22 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { X } from "lucide-react";
 
 export default function ProductPage() {
   const [categories, setCategories] = useState<string[]>([]);
+  const [newCategory, setNewCategory] = useState<string | undefined>();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
 
   const getCategories = async () => {
-    const result = await fetch("http://localhost:4000/categories");
+    const result = await fetch("http://localhost:4000/api/categories");
     const responseData = await result.json();
+    console.log({ responseData });
     const { data } = responseData;
+    console.log(data);
     setCategories(data);
   };
 
@@ -26,15 +33,54 @@ export default function ProductPage() {
     getCategories();
   }, []);
 
+  const newCategoryNameChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewCategory(e.target.value);
+  };
+  const createCategoryHandler = async () => {
+    await fetch("http://localhost:4000/api/categories", {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        newCategory,
+      }),
+    });
+    setModalOpen(false);
+    await getCategories();
+  };
+
+  const deleteCategoryHandler = async (category: string) => {
+    await fetch("http://localhost:4000/api/categories/delete", {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(category),
+    });
+  };
+
   return (
     <AdminLayout className="">
       <div className="flex flex-wrap gap-2">
         {categories.map((category) => (
-          <Badge variant={"outline"}>{category}</Badge>
+          <div
+            className="flex items-center border-2 rounded-full p-2 py-0"
+            key={category}
+          >
+            {category}
+            <X
+              className="hover:bg-gray-400/20 w-4"
+              onClick={() => deleteCategoryHandler(category)}
+            />
+          </div>
         ))}
-        <Dialog>
+        <Dialog open={modalOpen}>
           <DialogTrigger asChild>
             <Badge
+              onClick={() => setModalOpen(true)}
               variant={"outline"}
               className="cursor-pointer hover:bg-gray-500/20"
             >
@@ -44,11 +90,13 @@ export default function ProductPage() {
           <DialogContent className="w-[463px] p-6">
             <DialogHeader>
               <DialogTitle>Are you absolutely sure?</DialogTitle>
-              <DialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </DialogDescription>
             </DialogHeader>
+            <Input
+              type="text"
+              placeholder="new category"
+              onChange={newCategoryNameChangeHandler}
+            />
+            <Button onClick={createCategoryHandler}>Create</Button>
           </DialogContent>
         </Dialog>
       </div>
